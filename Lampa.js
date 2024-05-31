b82488c4e0ffb2a95462f90c05cbdcf0315dc892
Csorba Eszter <csorba.eszter.26@gmail.com>
2024-05-31 11:38:51 +0200

    kesz

--- Lampa.js
+++ Lampa.js
@@ -5,12 +5,13 @@ export default class Lampa {
     #szuloElem;
 
     constructor(allapot, id, szuloElem){
+       // console.log(allapot, id)
         //adattagok beallitasa
         this.#id = id; // ez fogja jelenteni, hogy hanyadik lampa a sorban
         this.#allapot = allapot; //fel v levannak kapcsolva
         this.#szuloElem = szuloElem; //amelyikbe belepakoljuk majd a kis divunket es meghivjuk a megjelenit metodust
         this.#megjelenit()
-        this.#divElem=this.#szuloElem.children("div.last-child") //szuloelem utolso divjet kell megfognunk
+        this.#divElem=this.#szuloElem.children("div:last-child") //szuloelem utolso divjet kell megfognunk
         this.#szinBeallit()
         this.#divElem.on("click", ()=> {
             this.#kattintasTrigger("kapcsolas")
